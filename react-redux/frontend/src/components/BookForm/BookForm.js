fe1e0ef0c243b7317abecc3cbe07e36b8ca752c8
Dmitry Chaiko <105819322+ChaikaDima@users.noreply.github.com>
2024-04-14 22:14:28 +0200

    Add files via upload

---
+++ react-redux/frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addBook } from '../../redux/books/actionCreators'
+import booksData from '../../data/books.json'
+import './BookForm.css'
+import createBookWithId from '../../utils/createBookWithId'
+
+const BookForm = () => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const dispatch = useDispatch()
+
+  const handleAddRandomBook = () => {
+    const randomIndex = Math.floor(Math.random() * booksData.length)
+    const randomBook = booksData[randomIndex]
+
+    const randomBookWithId = createBookWithId(randomBook)
+    dispatch(addBook(randomBookWithId))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (title && author) {
+      const book = createBookWithId({ title, author })
+      dispatch(addBook(book))
+      setTitle('')
+      setAuthor('')
+    }
+  }
+
+  return (
+    <div className="app-block book-form">
+      <h2>Add a new Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add Book</button>
+        <button type="button" onClick={handleAddRandomBook}>
+          Add Random
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default BookForm
